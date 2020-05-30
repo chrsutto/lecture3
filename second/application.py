ad92aaf0b203e70d87ba17ca8295920600e49189
Chris <chrsutto@gmail.com>
2020-05-30 14:15:15 -0700

    Added stuff

--- second/application.py
+++ second/application.py
@@ -1,7 +1,20 @@
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    headline = "Hello"
+    return render_template("index.html", headline=headline)
+
+@app.route("/bye")
+def bye():
+    headline ="Goodbye"
+    return render_template("index.html", headline=headline)
+
+@app.route("/newyear")
+def nye():
+    now = datetime.datetime.now()
+    new_year = now.month == 5 and now.day == 30
+    return render_template("nye.html", new_year=new_year)
